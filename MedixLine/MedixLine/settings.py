3094270569b3f21ee33351bfbde62053d7b20937
ahmednabil14 <ahmednabil14499@gmail.com>
2024-10-11 22:38:37 +0300

    postgresql

--- MedixLine/MedixLine/settings.py
+++ MedixLine/MedixLine/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,7 +155,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL='media/'
-import os
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
